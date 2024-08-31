f9cc8b50a96da2a93db68d412f72ca15c4660f45
sogaiu <983021772@users.noreply.github.com>
2024-08-31 21:34:47 +0900

    Provide non-interactive mode

--- build-and-run.sh
+++ build-and-run.sh
@@ -6,6 +6,8 @@ JANET_TAG=v1.35.2
 
 JANET_BIN=./janet/build/janet
 
+dir=$(pwd)
+
 ########################################################################
 
 # https://stackoverflow.com/a/27776822
@@ -35,35 +37,41 @@ fetch_and_build_janet()
     cd "$dir" || exit
 }
 
+main()
+{
+ if [ ! -f "$JANET_BIN" ]; then
+   fetch_and_build_janet
+ fi
+ cd "$dir" || exit
+ if [ -f "$JANET_BIN" ]; then
+   "$JANET_BIN" main.janet
+ else
+   printf "failed to find janet binary at: %s\n" "$JANET_BIN"
+   exit
+ fi
+}
+
 ########################################################################
 
-dir=$(pwd)
+if [ -n "$NON_INTERACTIVE" ]; then
+  main
+else
+  printf "Prepare a tree-sitter playground directory.\n"
+  printf "\n"
+  printf "This involves cloning, building, scraping, etc.\n"
+  printf "\n"
+  printf "Note: probably good to edit grammar-repos.txt first.\n"
+  printf "\n"
+  printf "Continue? [y/N] "
 
-printf "Prepare a tree-sitter playground directory.\n"
-printf "\n"
-printf "This involves cloning, building, scraping, etc.\n"
-printf "\n"
-printf "Note: probably good to edit grammar-repos.txt first.\n"
-printf "\n"
-printf "Continue? [y/N] "
+  read -r answer
 
-read -r answer
-
-case "$answer" in
-  y | Y | yes | YES)
-     if [ ! -f "$JANET_BIN" ]; then
-       fetch_and_build_janet
-     fi
-     cd "$dir" || exit
-     if [ -f "$JANET_BIN" ]; then
-       "$JANET_BIN" main.janet
-     else
-       printf "failed to find janet binary at: %s\n" "$JANET_BIN"
-       exit
-     fi
-    ;;
-  *)
-    printf "Ok, aborting.\n"
-    ;;
-esac
-
+  case "$answer" in
+    y | Y | yes | YES)
+      main
+      ;;
+    *)
+      printf "Ok, aborting.\n"
+      ;;
+  esac
+fi
